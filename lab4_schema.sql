f058427dbee71d30e4608fd033bff01f46ee85db
Oier Saizar <oierlas@gmail.com>
2016-12-08 19:15:49 +0100

    part 4 and part 5

--- lab4_schema.sql
+++ lab4_schema.sql
@@ -21,43 +21,44 @@ DROP TABLE IF EXISTS passenger_bookings;
 SELECT 'Creating tables' AS 'Message';
 
 CREATE TABLE route(
-  id INT,
-  dest VARCHAR(3),
-  source VARCHAR(3),
+  id INT AUTO_INCREMENT,
+  dest VARCHAR(3) NOT NULL,
+  source VARCHAR(3) NOT NULL,
   price DOUBLE,
+  year INT,
   CONSTRAINT pk_route PRIMARY KEY(id)
 );
 
 CREATE TABLE airport(
-  code VARCHAR(3),
+  code VARCHAR(3) NOT NULL,
   name VARCHAR(30),
-  city INT,
+  city INT NOT NULL,
   CONSTRAINT pk_airport PRIMARY KEY(code)
 );
 
 CREATE TABLE city(
-  id INT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(30),
-  country INT,
+  country INT NOT NULL,
   CONSTRAINT pk_city PRIMARY KEY(id)
 );
 
 CREATE TABLE country(
-  id INT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(30),
   CONSTRAINT pk_country PRIMARY KEY(id)
 );
 
 CREATE TABLE weekly_flight(
-  id INT,
-  weekday INT,
-  route INT,
+  id INT AUTO_INCREMENT,
+  weekday INT NOT NULL,
+  route INT NOT NULL,
   departure_time TIME,
   CONSTRAINT pk_wflight PRIMARY KEY(id)
 );
 
 CREATE TABLE week_day(
-  id INT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(10),
   year INT,
   wpfactor DOUBLE,
@@ -65,54 +66,56 @@ CREATE TABLE week_day(
 );
 
 CREATE TABLE flight(
-  id INT,
-  wflight INT,
+  id INT AUTO_INCREMENT,
+  wflight INT NOT NULL,
   week INT,
-  year INT,
+  year INT NOT NULL,
   CONSTRAINT pk_flight PRIMARY KEY(id)
 );
 
 CREATE TABLE year(
-  year INT,
+  year INT NOT NULL,
   ypfactor DOUBLE,
   CONSTRAINT pk_year PRIMARY KEY(year)
 );
 
 CREATE TABLE seat(
-  id INT,
+  id INT AUTO_INCREMENT,
   name VARCHAR(4),
+  flight  INT NOT NULL,
   CONSTRAINT pk_seat PRIMARY KEY(id)
 );
 
 CREATE TABLE ticket(
-  code INT,
-  seat INT,
-  passenger INT,
-  booking INT,
+  code INT NOT NULL,
+  seat INT NOT NULL,
+  passenger INT NOT NULL,
+  booking INT NOT NULL,
   CONSTRAINT pk_ticket PRIMARY KEY(code)
 );
 
 CREATE TABLE passenger(
+  id INT AUTO_INCREMENT,
   passport INT,
   name VARCHAR(30),
   surname VARCHAR(30),
-  CONSTRAINT pk_passenger PRIMARY KEY(code)
+  CONSTRAINT pk_passenger PRIMARY KEY(id)
 );
 
 CREATE TABLE booking(
-  code INT,
-  contact INT,
+  code INT NOT NULL,
+  contact INT NOT NULL,
   cphone BIGINT,
   cemail VARCHAR(30),
   payed BOOLEAN,
   price DOUBLE,
-  payedby INT,
-  flight INT,
+  payedby INT NOT NULL,
+  flight INT NOT NULL,
   CONSTRAINT pk_booking PRIMARY KEY(code)
 );
 
 CREATE TABLE ccinfo(
-  id INT,
+  id INT AUTO_INCREMENT,
   ccnumber BIGINT,
   name VARCHAR(30),
   surname VARCHAR(30),
@@ -120,8 +123,8 @@ CREATE TABLE ccinfo(
 );
 
 CREATE TABLE passenger_bookings(
-  passenger INT,
-  booking INT,
+  passenger INT NOT NULL,
+  booking INT NOT NULL,
   CONSTRAINT pk_passbook PRIMARY KEY(passenger,booking)
 );
 
@@ -129,3 +132,94 @@ SELECT 'Creating foreign keys' AS 'Message';
 
 ALTER TABLE route ADD CONSTRAINT fk_route_dest FOREIGN KEY (dest) REFERENCES airport(code);
 ALTER TABLE route ADD CONSTRAINT fk_route_source FOREIGN KEY (source) REFERENCES airport(code);
+ALTER TABLE airport ADD CONSTRAINT fk_airport_city FOREIGN KEY (city) REFERENCES city(id);
+ALTER TABLE city ADD CONSTRAINT fk_city_country FOREIGN KEY (country) REFERENCES country(id);
+ALTER TABLE weekly_flight ADD CONSTRAINT fk_weeklyflight_route FOREIGN KEY (route) REFERENCES route(id);
+ALTER TABLE weekly_flight ADD CONSTRAINT fk_weeklyflight_weekday FOREIGN KEY (weekday) REFERENCES week_day(id);
+ALTER TABLE flight ADD CONSTRAINT fk_flight_weekflight FOREIGN KEY (wflight) REFERENCES weekly_flight(id);
+ALTER TABLE flight ADD CONSTRAINT fk_flight_year FOREIGN KEY (year) REFERENCES year(year);
+ALTER TABLE seat ADD CONSTRAINT fk_seat_flight FOREIGN KEY (flight) REFERENCES flight(id);
+ALTER TABLE ticket ADD CONSTRAINT fk_ticket_seat FOREIGN KEY (seat) REFERENCES seat(id);
+ALTER TABLE ticket ADD CONSTRAINT fk_ticket_passenger FOREIGN KEY (passenger) REFERENCES passenger(id);
+ALTER TABLE ticket ADD CONSTRAINT fk_ticket_booking FOREIGN KEY (booking) REFERENCES booking(code);
+ALTER TABLE booking ADD CONSTRAINT fk_booking_ccinfo FOREIGN KEY (payedby) REFERENCES ccinfo(id);
+ALTER TABLE booking ADD CONSTRAINT fk_booking_contact FOREIGN KEY (contact) REFERENCES passenger(id);
+ALTER TABLE booking ADD CONSTRAINT fk_booking_flight FOREIGN KEY (flight) REFERENCES flight(id);
+ALTER TABLE passenger_bookings ADD CONSTRAINT fk_pb_bookings FOREIGN KEY (booking) REFERENCES booking(code);
+ALTER TABLE passenger_bookings ADD CONSTRAINT fk_pb_passenger FOREIGN KEY (passenger) REFERENCES passenger(id);
+
+SELECT 'Creating help functions (part4)' AS 'Message';
+
+DROP FUNCTION IF EXISTS calculateFreeSeats;
+
+DELIMITER $$
+CREATE FUNCTION calculateFreeSeats(flight INT)
+  RETURNS INT
+BEGIN
+  DECLARE seats INT;
+  select 40-COUNT(*)
+  INTO @seats
+  from booking
+  where booking.flight = flight and booking.payed = 0;
+  RETURN seats;
+
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION calculatePrice(flight INT)
+  RETURNS DOUBLE
+BEGIN
+  DECLARE totalprice DOUBLE;
+  DECLARE seats INT;
+  DECLARE wfactor DOUBLE;
+  DECLARE yfactor DOUBLE;
+  DECLARE routeprice DOUBLE;
+
+  select COUNT(*)
+  INTO @seats
+  from booking
+  where booking.flight = flight and booking.payed = 0;
+
+  select week_day.wpfactor
+  INTO @wfactor
+  from flight
+  join weekly_flight on flight.wflight = weekly_flight.id
+  join week_day on weekly_flight.weekday = week_day.id
+  where week_day.year = flight.year and flight.id = flight;
+
+  select year.ypfactor
+  into @yfactor
+  from flight
+  join year on flight.year = year.year
+  where flight.id = flight;
+
+  select route.price
+  into @routeprice
+  from flight
+  join weekly_flight on flight.wflight = weekly_flight.id
+  join route on weekly_flight.route = route.id
+  where flight.id = flight;
+
+  SET totalprice = routeprice * wfactor * (seats+1)/40 * yfactor;
+  RETURN totalprice;
+
+END;
+$$
+DELIMITER ;
+
+SELECT 'Creating triggers (part 5)' AS 'Message';
+
+DELIMITER $$
+CREATE TRIGGER before_ticket_insert
+    BEFORE INSERT ON ticket
+    FOR EACH ROW
+BEGIN
+    SET NEW.code = RAND();
+END$$
+DELIMITER ;
+
+SELECT 'Creating reservation stored procedures (part 6)' AS 'Message';
+
+SELECT 'Creating custom views (part 7)' AS 'Message';
