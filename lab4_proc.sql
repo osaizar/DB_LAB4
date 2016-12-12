9fde10378edf642e83d6740830fcb3bbd7f18dbe
jdelcano <delcanovega@gmail.com>
2016-12-12 17:53:19 +0100

    Procedures fixed

--- lab4_proc.sql
+++ lab4_proc.sql
@@ -38,22 +38,26 @@ delimiter //
 CREATE PROCEDURE addFlight(IN deptcode VARCHAR(3), IN arrcode VARCHAR(3), IN yr INT, IN day VARCHAR(10), IN dtime TIME)
 BEGIN
 
-DECLARE rt INT = (SELECT id FROM route WHERE ((dest LIKE upper(deptcode)) AND (source LIKE upper(arrcode)) AND (year = yr)));
+DECLARE rt INT;
+DECLARE cnt INT;
+
+SELECT id INTO @rt FROM route WHERE ((dest LIKE upper(deptcode)) AND (source LIKE upper(arrcode)) AND (year = yr));
 INSERT INTO weekly_flight (weekday, route, departure_time)
 VALUES ((SELECT id FROM week_day WHERE name LIKE lower(day)),
         @rt,
         dtime);
 
-DECLARE cnt INT = 1;
+
+SET @cnt = 1;
 WHILE @cnt < 53
-BEGIN
+DO
    INSERT INTO flight (wflight, week, year)
    VALUES ((SELECT id FROM weekly_flight WHERE route = @rt),
            @cnt,
            year);
 
    SET @cnt = @cnt + 1;
-END;
+END WHILE;
 
 END; //
 delimiter ;
