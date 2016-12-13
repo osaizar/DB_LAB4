ef39312687db1f5397d8ea3a7de964fe030cabc8
jdelcano <delcanovega@gmail.com>
2016-12-13 17:44:22 +0100

    addFlight() cleaned

--- lab4_proc.sql
+++ lab4_proc.sql
@@ -44,9 +44,13 @@ delimiter //
 CREATE PROCEDURE addFlight(IN deptcode VARCHAR(3), IN arrcode VARCHAR(3), IN yr INT, IN day VARCHAR(10), IN dtime TIME)
 BEGIN
 
-DECLARE rt INT;
-DECLARE cnt INT;
-DECLARE cnts INT;
+DECLARE rt INT;          -- Route ID
+DECLARE cnt INT;         -- Count for the flights (52)
+DECLARE cnts INT;        -- Count fot the seats (40)
+DECLARE wflight_id INT;  -- Weekly flight ID
+DECLARE flight_id INT;   -- Flight ID
+
+-- WEEKLY FLIGHT INSERTION:
 
 SELECT id INTO @rt FROM route WHERE ((dest LIKE upper(deptcode)) AND (source LIKE upper(arrcode)) AND (year = yr));
 INSERT INTO weekly_flight (weekday, route, departure_time)
@@ -55,26 +59,33 @@ VALUES ((SELECT id FROM week_day WHERE name LIKE lower(day)),
         dtime);
 
 
+-- NOW THE 52 INSTANCES OF THAT FLIGHT:
+
+SELECT id INTO @wflight_id FROM weekly_flight WHERE route = @rt;
+
 SET @cnt = 1;
 SET @cnts = 1;
 WHILE @cnt < 53
 DO
+
    INSERT INTO flight (wflight, week, year)
-   VALUES ((SELECT id FROM weekly_flight WHERE route = @rt),
-           @cnt,
-           yr);
+   VALUES (@wflight_id, @cnt, yr);
+
+   -- AND THE 40 SEATS FOR EACH FLIGHT:
+
+   SELECT id INTO @flight_id FROM flight WHERE week = @cnt AND year = yr AND wflight = @wflight_id;
+
    WHILE @cnts < 41
    DO
-      INSERT INTO seat (name,flight)
-      VALUES(@cnts,
-            (SELECT id  FROM flight WHERE week = @cnt and year = yr and
-            wflight = (SELECT id FROM weekly_flight WHERE route = @rt)));
+      INSERT INTO seat (name, flight)
+      VALUES (@cnts, @flight_id);
+
       SET @cnts = @cnts + 1;
-      END WHILE;
+   END WHILE;  -- Seats while
 
    SET @cnts = 1;
    SET @cnt = @cnt + 1;
-END WHILE;
+END WHILE;  -- Flights while
 
 END; //
 delimiter ;
